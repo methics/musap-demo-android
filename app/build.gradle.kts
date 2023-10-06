f13af70a93cd93eb6d19a9c967cbd7d6a03dcc76
Atte <waldenatte@gmail.com>
2023-10-06 16:33:41 +0300

    Add JWT lib for dev purposes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation("com.github.tony19:logback-android:3.0.0")
     implementation("com.yubico.yubikit:piv:2.3.0")
     implementation("org.bouncycastle:bcpkix-jdk15to18:1.71")
+    implementation (files("libs/nimbus-jose-jwt-9.21.jar"))
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
